65cf753f32eb9f04382c448aa2a2d77e43c86df8
Jesús Germade <jesus@germade.es>
2019-04-09 23:54:46 +0200

    some example tweaks

--- example/static/example.js
+++ example/static/example.js
@@ -1,23 +1,24 @@
 
 (window.aplazame = window.aplazame || []).push(function (aplazame) {
-  var checkoutData,
-      onSuccess = function () {
+  var onSuccess = function () {
         console.log('confirmed!');
       },
       onError = function () {
         console.log('order canceled!');
       }
 
-  document.querySelector('[data-aplazame-button]')
-    .addEventListener('click', function () {
+  var button_el = document.querySelector('[data-aplazame-button]')
+  button_el.setAttribute('original-text', button_el.textContent)
+  
+  button_el.addEventListener('click', function () {
+    button_el.textContent = 'iniciando checkout...'
+    button_el.setAttribute('disabled', '')
 
-      ( checkoutData
-          ? Promise.resolve(checkoutData)
-          : fetch('/checkout/order').then(function (res) {
-              return res.json()
-            })
-      ).then(function (checkout_data) {
-
+    fetch('/checkout/order')
+      .then(function (res) {
+        return res.json()
+      })
+      .then(function (checkout_data) {
         console.log('checkout', checkout_data)
 
         aplazame.checkout(checkout_data, {
@@ -25,6 +26,10 @@
           onDismiss: onError,
         })
 
+      }, console.error)
+      .then(function () {
+        button_el.textContent = button_el.getAttribute('original-text')
+        button_el.removeAttribute('disabled')
       })
-    })
+  })
 })
